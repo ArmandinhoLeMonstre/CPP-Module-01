23194ed104990d2f68c64363f84c072f3fec6e20
Armand Mititelu <armitite@fu-r8-p8.s19.be>
2025-02-01 22:02:56 +0100

    A faire

---
+++ ex00/Zombie.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armitite <armitite@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/01 20:59:25 by armitite          #+#    #+#             */
+/*   Updated: 2025/02/01 21:20:11 by armitite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+#include <string>
+#include <iostream>
+
+void	Zombie::announce(void) {
+
+	std::cout << "Braaiz" << std::endl;
+
+	return ;
+}
+
+Zombie::Zombie(void) {
+
+	return ;
+}
+
+Zombie::~Zombie(void) {
+
+	return ;
+}
