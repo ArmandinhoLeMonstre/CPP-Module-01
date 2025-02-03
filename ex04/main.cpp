951af042387f0a4eef85eb3efdac6c6e915b8b4f
Armand Mititelu <armitite@mi-c2-p7.s19.be>
2025-02-03 14:06:50 +0100

    chui trop chaud

---
+++ ex04/main.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armitite <armitite@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 12:09:58 by armitite          #+#    #+#             */
+/*   Updated: 2025/02/03 14:05:45 by armitite         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstring>
+
+int	main(void)
+{
+	std::string word = "alo";
+	std::string replace = "juliano";
+	std::ifstream ifs("text", std::ios::binary);
+    if (!ifs) {
+        std::cerr << "Error opening file!" << std::endl;
+        return 1;
+    }
+
+    std::ostringstream oss;
+    oss << ifs.rdbuf();
+    std::string content = oss.str();
+	std::ofstream	ofs("outfile");
+	std::size_t found = content.find(word);
+	while (found != std::string::npos)
+	{
+		std::cout << "Le found: " << found << std::endl;
+		for (unsigned i = 0; i < found; i++)
+		{
+			ofs << content.at(i);
+		}
+		for (unsigned i = 0; i < strlen(replace.c_str()); i++)
+		{
+			ofs << replace.at(i);
+		}
+		content.erase(0, found + strlen(word.c_str()));
+		found = content.find(word);
+	}
+	ifs.close();
+	ofs.close();
+	return (0);
+}
